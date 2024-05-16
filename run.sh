94c70b94d26ec4d5c72b786e3c88c4d2f500ffde
trapoom555 <trapoom555@gmail.com>
2024-05-16 20:34:35 +0800

    change cmd

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 export CUDA_VISIBLE_DEVICES=1
 
-python main.py --config configs/text_mv.yaml prompt="An obese cat" save_path=corgi --wogui
+python main.py --config configs/text_mv.yaml prompt="An obese cat" --wogui
